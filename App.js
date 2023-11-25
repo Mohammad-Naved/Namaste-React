e058d4282c46d7761f61bc8dea440b87f5ea5455
Mohammad-Naved <knaved782@gmail.com>
2023-11-25 12:16:26 +0530

    homeworkAdded as comment

--- App.js
+++ App.js
@@ -11,6 +11,16 @@ const FunctionalComponent=()=>(
     <h1 id="heading">This is functional component</h1>
   </div>
 )
+
+//HomeWork
+
+// const Header=()=>(
+//   <div className="flex">
+//     <img className="logo-image" src={"https:media.istockphoto.com/id/1313644269/vector/gold-and-silver-circle-star-logo-template.jpg?s=612x612&w=0&k=20&c=hDqCI9qTkNqNcKa6XS7aBim7xKz8cZbnm80Z_xiU2DI="}></img>
+//   <input type="text" placeholder="Search.."></input>
+//   <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSqWNCu5PKphREwyLSVJcWREXqe-9ny7kzSmjE9SmsfA&s" }/>
+//   </div>
+//   )
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<FunctionalComponent/>);
