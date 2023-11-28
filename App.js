a362c1dd71d3492ab28f085faa4d567a80826dde
Mohammad-Naved <knaved782@gmail.com>
2023-11-28 23:24:37 +0530

    removed unused code

--- App.js
+++ App.js
@@ -1,27 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-
-//React Element
-const heading = <h1 id="heading">Salam React By JSX</h1>;
-
-//React Component
-const Title = () => <h1>Title</h1>;
-//React component
-const HeadingComponent = () => (
-  <div>
-        <Title />
-    <h1 id="heading">This is functional Component</h1>
-  </div>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-
-    <HeadingComponent />
-);
-=======
 
 //Header
 //  Logo
@@ -807,27 +785,6 @@ const AppLayout = () => {
   );
 };
 
-// const Heading =()=> (<h1>Hello from functional component</h1>);
-
-// const FunctionalComponent=()=>(
-//   <div>
-//     {Heading()}
-//     <Heading/>
-//     <Heading></Heading>
-//     <h1 id="heading">This is functional component</h1>
-//   </div>
-// )
-
-// //HomeWork
-
-// // const Header=()=>(
-// //   <div className="flex">
-// //     <img className="logo-image" src={"https:media.istockphoto.com/id/1313644269/vector/gold-and-silver-circle-star-logo-template.jpg?s=612x612&w=0&k=20&c=hDqCI9qTkNqNcKa6XS7aBim7xKz8cZbnm80Z_xiU2DI="}></img>
-// //   <input type="text" placeholder="Search.."></input>
-// //   <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSqWNCu5PKphREwyLSVJcWREXqe-9ny7kzSmjE9SmsfA&s" }/>
-// //   </div>
-// //   )
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<AppLayout />);
->>>>>>> feature
