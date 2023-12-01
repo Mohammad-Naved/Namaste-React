0992cc78a632dc17b7b4261af3d3b4ed227d547d
Mohammad-Naved <knaved782@gmail.com>
2023-12-02 03:11:54 +0530

    added filter top resturant

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,45 @@
 import ResturantCard from "./ResturantCard";
 import resLIST from "../utility/apiData";
 
-const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="Enter your Resturant name"
-          ></input>
-          <button className="search-btn" type="submit">
-            Search
-          </button>
-        </div>
-        <div className="resCard">
-          {resLIST.map((resturant) => (
-            <ResturantCard key={resturant.info.id} resOBJ={resturant} />
-          ))}
-        </div>
-      </div>
-    );
-  };
+import { useState } from "react";
 
-  export default Body;
+const Body = () => {
+const [resturantListData,setResturantListData]=useState(resLIST);
+
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          type="submit"
+          onClick={() => {
+            const filteredRes=resLIST.filter((resLIST) => resLIST.info.avgRating > 4);
+            setResturantListData(filteredRes);
+            
+          }}
+        >
+          Top rated resturant
+        </button>
+        <button
+          className="filter-btn"
+          type="submit"
+          onClick={() => {
+            
+            setResturantListData(resLIST);
+            
+          }}
+        >
+          All Resturant
+        </button>
+      </div>
+      <div className="resCard">
+        {resturantListData.map((resturant) => (
+          <ResturantCard key={resturant.info.id} resOBJ={resturant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -1,19 +1,23 @@
 const ResturantCard = (props) => {
-    const {resOBJ}=props;
-    const {cloudinaryImageId, name, cuisines, costForTwo, sla, avgRating}=resOBJ?.info;
-    return (
-      <div className="res-card">
-        <img
-          className="res-logo"
-          src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}
-        ></img>
-        <h3>{name}</h3>
-        <h4>{avgRating} stars</h4>
-        <h4>{cuisines.join(", ")}</h4>
-        <h4>{costForTwo} </h4>
-        <h4>{sla.deliveryTime} minutes </h4>
-      </div>
-    );
-  };
+  const { resOBJ } = props;
+  const { cloudinaryImageId, name, cuisines, costForTwo, sla, avgRating } =
+    resOBJ?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h3>{name}</h3>
+      <h4>{avgRating} stars</h4>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{costForTwo} </h4>
+      <h4>{sla.deliveryTime} minutes </h4>
+    </div>
+  );
+};
 
-  export default ResturantCard;
+export default ResturantCard;
