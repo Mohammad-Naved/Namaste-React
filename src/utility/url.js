682aa505f5e3d4952f00c0c37955b9ff25b66015
Mohammad-Naved <knaved782@gmail.com>
2023-12-10 19:37:43 +0530

    Added Swiggy API

---
+++ src/utility/url.js
@@ -0,0 +1,1 @@
+export const SWIGGYAPI="https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
